8e4a0c026afa5c69728b39abe4a3fe3b29b2dacb
Ishan Khare <ishankhare07@gmail.com>
2015-11-23 12:05:11 +0530

    final design ready

--- main.js
+++ main.js
@@ -1,31 +1,47 @@
 var app = angular.module('tracker', []);
 
 app.controller('mapController', function() {
-    angular.element(document).ready(function () {
+    angular.element(document).ready(function() {
         map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: -34.397, lng: 150.644},
+            center: {
+                lat: -34.397,
+                lng: 150.644
+            },
             zoom: 8
         });
+
+        var marker = new google.maps.Marker({
+            map: map,
+            position: {lat: -34.397,
+            lng: 150.644},
+        });
     });
+
+    this.updateLocation = function(newLocation) {
+        console.log(newLocation);
+        map.panTo(google.maps.LatLang(newLocation.latitude, newLocation.longitude));
+        //marker.position = newLocation;
+    }
 });
 
-app.controller('parcelController',['$interval', '$http', function($interval, $http) {
+app.controller('parcelController', ['$interval', '$http', function($interval, $http) {
     var store = this;
     store.parcels = [];
     store.api_count = 0;
+    store.currentSelected = null;
 
     this.update_parcels = function() {
         var req = $http.get('http://zoomcar-ui.0x10.info/api/courier?type=json&query=list_parcel')
-        .success(function(data) {
-            store.parcels = data.parcels;
-        });
+            .success(function(data) {
+                store.parcels = data.parcels;
+            });
     };
 
     this.check_api_hits = function() {
         var req = $http.get('http://zoomcar-ui.0x10.info/api/courier?type=json&query=api_hits')
-        .success(function(data) {
-            store.api_count = data.api_hits;
-        });
+            .success(function(data) {
+                store.api_count = data.api_hits;
+            });
     };
 
     $interval(this.check_api_hits, 1000);
